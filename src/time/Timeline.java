e8046534bd441ff28e523cdab11fc4debb68d2f6
bettytuan0209 <bettytuan0209@hotmail.com>
2013-11-16 17:18:17 -0600

    Timeline added intersection and tested

--- src/time/Timeline.java
+++ src/time/Timeline.java
@@ -9,6 +9,7 @@ import org.joda.time.DateTime;
 import org.joda.time.Duration;
 import org.joda.time.Interval;
 
+import schedulable.Activity;
 import schedulable.LegalTime;
 import schedulable.Schedulable;
 import util.DeepCopy;
@@ -94,7 +95,7 @@ public class Timeline implements Serializable {
 		
 		while (true) {
 			// out of the interval of this timeline
-			if (start.isAfter(interval.getEnd())) {
+			if (!start.isBefore(interval.getEnd())) {
 				return null;
 			}
 			
@@ -146,6 +147,86 @@ public class Timeline implements Serializable {
 			}
 		}
 		
+	}
+	
+	public Timeline intersect(Timeline another) {
+		Timeline intersection;
+		
+		// check for boundary
+		if (isEmpty()
+				|| another.isEmpty()
+				|| Util.getEndTime(schedule.lastEntry()).isBefore(
+						another.schedule.firstKey())
+				|| Util.getEndTime(another.schedule.lastEntry()).isBefore(
+						schedule.firstKey())) {
+			return new Timeline(new Interval(0, 0));
+		}
+		
+		DateTime latestStart = later(schedule.firstKey(),
+				another.schedule.firstKey());
+		DateTime earliestEnd = earlier(Util.getEndTime(schedule.lastEntry()),
+				Util.getEndTime(another.schedule.lastEntry()));
+		intersection = new Timeline(new Interval(latestStart, earliestEnd));
+		
+		Iterator<Map.Entry<DateTime, Schedulable>> mineItr = schedule
+				.entrySet().iterator();
+		Iterator<Map.Entry<DateTime, Schedulable>> theirItr = another.schedule
+				.entrySet().iterator();
+		
+		Map.Entry<DateTime, Schedulable> mine = null;
+		Map.Entry<DateTime, Schedulable> their = null;
+		
+		while (true) {
+			
+			// this block of mine is irrelevant now
+			if (mine == null
+					|| (their != null && Util.getEndTime(mine).isBefore(
+							their.getKey()))) {
+				if (mineItr.hasNext()) {
+					mine = mineItr.next();
+					continue;
+				} else {
+					return intersection;
+				}
+			}
+			
+			// this block of their is irrelevant now
+			if (their == null || Util.getEndTime(their).isBefore(mine.getKey())) {
+				if (theirItr.hasNext()) {
+					their = theirItr.next();
+					continue;
+				} else {
+					return intersection;
+				}
+			}
+			
+			// schedule intersection
+			latestStart = later(mine.getKey(), their.getKey());
+			earliestEnd = earlier(Util.getEndTime(mine), Util.getEndTime(their));
+			if (!intersection.schedule(latestStart, new Activity(new Duration(
+					latestStart, earliestEnd)))) {
+				throw new UnsupportedOperationException(
+						"Cannot insert intersection.");
+			}
+			
+			// forward one of the schedulables
+			if (earliestEnd.equals(Util.getEndTime(mine))) {
+				if (mineItr.hasNext()) {
+					mine = mineItr.next();
+					continue;
+				} else {
+					return intersection;
+				}
+			} else {
+				if (theirItr.hasNext()) {
+					their = theirItr.next();
+					continue;
+				} else {
+					return intersection;
+				}
+			}
+			
+		}
 	}
 	
 	public boolean schedule(DateTime startTime, Schedulable schedulable) {
@@ -221,6 +302,14 @@ public class Timeline implements Serializable {
 	
 	private DateTime later(DateTime first, DateTime second) {
 		if (first.isAfter(second)) {
+			return first;
+		} else {
+			return second;
+		}
+	}
+	
+	private DateTime earlier(DateTime first, DateTime second) {
+		if (first.isBefore(second)) {
 			return first;
 		} else {
 			return second;
