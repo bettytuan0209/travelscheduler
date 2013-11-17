7a51c4be7c8d02abecac5ad8cd0e337a668b3d10
bettytuan0209 <bettytuan0209@hotmail.com>
2013-11-16 18:00:08 -0600

    AST add Activity and edge implemented, not tested

--- src/activities/ActivitySpanningTree.java
+++ src/activities/ActivitySpanningTree.java
@@ -3,38 +3,73 @@ package activities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.TreeMap;
 
 import org.jgraph.graph.Edge;
 import org.jgrapht.graph.SimpleGraph;
+import org.joda.time.DateTime;
 import org.joda.time.Duration;
+import org.joda.time.Interval;
 
 import schedulable.Activity;
+import schedulable.Schedulable;
+import time.LegalTimeline;
+import time.TimeBlock;
 
 public class ActivitySpanningTree implements Serializable {
 	private static final long serialVersionUID = 6797165864508180241L;
 	public int index;
-	public ArrayList<Integer> availableTBs;
+	public ArrayList<TimeBlock> availableTBs;
 	private SimpleGraph<Activity, Edge> tree;
 	private Duration sumActivitiesTime;
-
-	public ActivitySpanningTree(int index) {
+	
+	public ActivitySpanningTree(int index, ArrayList<TimeBlock> availableTBs,
+			SimpleGraph<Activity, Edge> tree) {
 		this.index = index;
+		this.availableTBs = availableTBs;
+		this.tree = tree;
 	}
-
-	public boolean addActivity() {
-		// update sumActivitiesTime and check if possible to add
+	
+	public boolean addActivity(Activity activity) {
+		
+		// check for commonTBs
+		ArrayList<Integer> commonTBs = new ArrayList<Integer>();
+		for (TimeBlock tb : availableTBs) {
+			TreeMap<DateTime, Schedulable> map = new TreeMap<DateTime, Schedulable>();
+			Interval interval = tb.getInterval();
+			map.put(new DateTime(interval.getStart()),
+					new Activity(interval.toDuration()));
+			Activity tester = new Activity("tester", activity.getDuration(),
+					activity.legalTimeline.intersect(new LegalTimeline(map)));
+			if (tester.forwardChecking()) {
+				commonTBs.add(tb.getIndex());
+			}
+		}
+		if (commonTBs.isEmpty()) {
+			return false;
+		}
+		
+		tree.addVertex(activity);
+		
+		// update sumActivitiesTime
+		sumActivitiesTime = sumActivitiesTime.plus(activity.getDuration());
+		
 		return true;
 	}
-
+	
+	public boolean addEdge(Activity origin, Activity dest, Edge edge) {
+		return tree.addEdge(origin, dest, edge);
+	}
+	
 	public Set<Activity> getActivities() {
 		return tree.vertexSet();
-
+		
 	}
-
+	
 	public Duration getSumActivitiesTime() {
 		return sumActivitiesTime;
 	}
-
+	
 	public boolean removeActivity(Activity activity) {
 		if (tree.removeVertex(activity)) {
 			sumActivitiesTime = sumActivitiesTime.minus(activity.getDuration());
@@ -42,5 +77,5 @@ public class ActivitySpanningTree implements Serializable {
 		}
 		return false;
 	}
-
+	
 }
