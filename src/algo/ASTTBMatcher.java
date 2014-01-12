d09aaf330bb5390ba2a52f30e6575a31a8fcfd89
bettytuan0209 <bettytuan0209@hotmail.com>
2014-01-12 13:49:41 -0600

    Source code for the Matching module

---
+++ src/algo/ASTTBMatcher.java
@@ -0,0 +1,39 @@
+package algo;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import schedulable.Transportation;
+import search.DFS;
+import search.TreeSearch;
+import state.MatchingState;
+import time.TimeBlock;
+import activities.ActivitySpanningTree;
+import activities.Location;
+
+public class ASTTBMatcher {
+	
+	public static ArrayList<TimeBlock> matching(
+			SimpleWeightedGraph<Location, Transportation> graph,
+			Set<ActivitySpanningTree> asts) {
+		
+		// construct the initial state
+		MatchingState root = new MatchingState(asts);
+		
+		TreeSearch searcher = new TreeSearch(new DFS(), root);
+		
+		// for each goal state, pass to next module and wait for response
+		MatchingState goal;
+		while ((goal = (MatchingState) searcher.nextGoal()) != null) {
+			ArrayList<TimeBlock> schedule = Scheduler.autoScheduleAll(graph,
+					goal.getPairs());
+			if (schedule != null) {
+				return schedule;
+			}
+		}
+		
+		return null;
+	}
+}
---
+++ src/state/MatchingState.java
@@ -0,0 +1,68 @@
+package state;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+import time.TimeBlock;
+import util.DeepCopy;
+import activities.ActivitySpanningTree;
+
+public class MatchingState implements SearchState, Serializable {
+	
+	private static final long serialVersionUID = 4188017693668337015L;
+	private Set<ActivitySpanningTree> asts;
+	private HashMap<TimeBlock, ActivitySpanningTree> pairs;
+	
+	public MatchingState(Set<ActivitySpanningTree> asts) {
+		this.asts = asts;
+		pairs = new HashMap<TimeBlock, ActivitySpanningTree>();
+	}
+	
+	@Override
+	public ArrayList<SearchState> successors() {
+		ArrayList<SearchState> successors = new ArrayList<SearchState>();
+		
+		// handle the next ast in the set
+		Iterator<ActivitySpanningTree> itr = asts.iterator();
+		if (itr.hasNext()) {
+			ActivitySpanningTree ast = itr.next();
+			asts.remove(ast);
+			
+			// for each tb that this ast can match with
+			for (TimeBlock tb : ast.availableTBs) {
+				
+				// if this tb hasn't been matched with another ast
+				if (!pairs.containsKey(tb)) {
+					
+					// create a new state with that match and insert to
+					// successors
+					MatchingState newState = (MatchingState) DeepCopy
+							.copy(this);
+					
+					if (newState.pairs.put(tb, ast) != null) {
+						successors.add(newState);
+					}
+					;
+				}
+			}
+			
+		} else {
+			throw new IllegalStateException("Unexceptedly out of tree nodes");
+		}
+		
+		return successors;
+	}
+	
+	@Override
+	public boolean checkGoal() {
+		return asts.isEmpty();
+	}
+	
+	public HashMap<TimeBlock, ActivitySpanningTree> getPairs() {
+		return pairs;
+	}
+	
+}
