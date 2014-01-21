3f386f6f85e2fe3a4b7d5f8e155dd34b404af134
bettytuan0209 <bettytuan0209@hotmail.com>
2014-01-20 18:44:05 -0600

    Javadoc documentation

--- src/algo/ASTTBMatcher.java
+++ src/algo/ASTTBMatcher.java
@@ -13,8 +13,26 @@ import time.TimeBlock;
 import activities.ActivitySpanningTree;
 import activities.Location;
 
+/**
+ * Automatically matches each AST with a TB
+ * 
+ * @author chiao-yutuan
+ * 
+ */
 public class ASTTBMatcher {
 	
+	/**
+	 * Matches each AST with a TB in its availableTBs list, then pass to
+	 * scheduler. If scheduler failed, try to find the next possible match
+	 * config
+	 * 
+	 * @param graph
+	 *            The graph to pass on to the scheduler
+	 * @param asts
+	 *            Set of asts to match with. Their data structure contains the
+	 *            availableTBs
+	 * @return Result of the scheduler as an arraylist of timeblocks
+	 */
 	public static ArrayList<TimeBlock> matching(
 			SimpleWeightedGraph<Location, Transportation> graph,
 			Set<ActivitySpanningTree> asts) {
